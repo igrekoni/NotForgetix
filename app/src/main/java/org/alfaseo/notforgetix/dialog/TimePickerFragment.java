ed0646783c03175ba3a5c2ba38a8cd4fd0dadb37
igrekoni <igrekoni@gmail.com>
2016-03-16 15:08:52 +0300

    третье видео 20:10

---
+++ app/src/main/java/org/alfaseo/notforgetix/dialog/TimePickerFragment.java
@@ -0,0 +1,24 @@
+package org.alfaseo.notforgetix.dialog;
+
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.widget.TimePicker;
+
+/**
+ * Created by Gre on 16.03.2016.
+ */
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener{
+
+
+
+
+
+
+
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+    }
+}
