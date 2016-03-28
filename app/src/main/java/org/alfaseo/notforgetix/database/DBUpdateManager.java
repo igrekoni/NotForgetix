ea579e80d6c8d9b8cc31de1976510e39993e266c
igrekoni <igrekoni@gmail.com>
2016-03-28 19:15:18 +0300

    шестое видео 14:40

---
+++ app/src/main/java/org/alfaseo/notforgetix/database/DBUpdateManager.java
@@ -0,0 +1,13 @@
+package org.alfaseo.notforgetix.database;
+
+import android.content.ContentValues;
+
+/**
+ * Created by Gre on 28.03.2016.
+ */
+public class DBUpdateManager {
+
+    private void update(String column, long key, String value){
+        ContentValues cv = new ContentValues();
+    }
+}
--- app/src/main/java/org/alfaseo/notforgetix/model/ModelTask.java
+++ app/src/main/java/org/alfaseo/notforgetix/model/ModelTask.java
@@ -2,6 +2,8 @@ package org.alfaseo.notforgetix.model;
 
 import org.alfaseo.notforgetix.R;
 
+import java.util.Date;
+
 /**
  * Created by Gre on 18.03.2016.
  */
@@ -22,17 +24,20 @@ public class ModelTask implements Item {
     private long date;
     private int priority;
     private int status;
+    private long timeStamp;
 
 
     public ModelTask(){
         this.status = -1;
+        this.timeStamp = new Date().getTime();
     }
 
-    public ModelTask(String title, long date, int priority, int status) {
+    public ModelTask(String title, long date, int priority, int status, long timeStamp) {
         this.title = title;
         this.date = date;
         this.priority = priority;
         this.status = status;
+        this.timeStamp = timeStamp;
     }
 
 
@@ -94,5 +99,13 @@ public class ModelTask implements Item {
 
     public void setPriority(int priority) {
         this.priority = priority;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
     }
 }
