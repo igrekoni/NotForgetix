e244960e119be6d52a38f3b02152b419eec3b333
igrekoni <igrekoni@gmail.com>
2016-05-05 18:43:26 +0300

    девятое видео закончено

---
+++ app/src/main/java/org/alfaseo/notforgetix/model/ModelSeparator.java
@@ -0,0 +1,11 @@
+package org.alfaseo.notforgetix.model;
+
+/**
+ * Created by Gre on 05.05.2016.
+ */
+public class ModelSeparator {
+    public static final int TYPE_
+    public static final int TYPE_
+    public static final int TYPE_
+    public static final int TYPE_
+}
