4476ca84a03b2d3e620c76fbc266fdbe24fd8e58
igrekoni <igrekoni@gmail.com>
2016-05-25 16:00:10 +0300

    иконка / цвета

--- app/src/main/java/org/alfaseo/notforgetix/model/ModelSeparator.java
+++ app/src/main/java/org/alfaseo/notforgetix/model/ModelSeparator.java
@@ -6,7 +6,7 @@ import org.alfaseo.notforgetix.R;
  * Created by Gre on 05.05.2016.
  */
 public class ModelSeparator implements Item {
-    public static final int TYPE_OVERDUE = R.string.separator_overue;
+    public static final int TYPE_OVERDUE = R.string.separator_overdue;
     public static final int TYPE_TODAY = R.string.separator_today;
     public static final int TYPE_TOMORROW = R.string.separator_tomorrow;
     public static final int TYPE_FUTURE = R.string.separator_future;
